b671e129765a1292ea6cca1a7aa5c4fcdb6c3225
Glenn Ramalho <glennramalho@yahoo.de>
2020-06-16 17:47:31 -0300

    Added a wait with no timeout. This could be done without it, but it was not very elegant. This is better.

--- src/modules/gn_semaphore.h
+++ src/modules/gn_semaphore.h
@@ -4,12 +4,18 @@ class gn_semaphore : public sc_semaphore {
    public:
    gn_semaphore(int iv): sc_semaphore(iv) {}
    gn_semaphore(const char *n, int iv): sc_semaphore(n, iv) { }
+   virtual int wait() {
+      return sc_semaphore::wait();
+   }
    virtual int wait(const sc_time &timeout) {
       while( in_use() ) {
 	sc_core::wait( timeout , m_free );
       }
-      if (m_free.triggered()) -- m_value;
-      return 0;
+      if (m_free.triggered()) {
+         -- m_value;
+         return 0;
+      }
+      else return -1;
    }
    virtual const char* kind() const
       { return "gn_semaphore"; }
