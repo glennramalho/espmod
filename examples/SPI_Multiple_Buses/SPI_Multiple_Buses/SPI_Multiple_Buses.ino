4ea1dcd2faa3c82bd9a70b40c763f3416f8d20a3
Glenn Ramalho <glennramalho@yahoo.de>
2020-06-19 16:07:40 -0300

    Added a command to sample the values from the VSPI and HSPI. Updated the header.

--- examples/SPI_Multiple_Buses/SPI_Multiple_Buses/SPI_Multiple_Buses.ino
+++ examples/SPI_Multiple_Buses/SPI_Multiple_Buses/SPI_Multiple_Buses.ino
@@ -1,15 +1,19 @@
 /* The ESP32 has four SPi buses, however as of right now only two of
- * them are available to use, HSPI and VSPI. Simply using the SPI API 
+ * them are available to use, HSPI and VSPI. Simply using the SPI API
  * as illustrated in Arduino examples will use VSPI, leaving HSPI unused.
- * 
+ *
  * However if we simply intialise two instance of the SPI class for both
  * of these buses both can be used. However when just using these the Arduino
  * way only will actually be outputting at a time.
- * 
+ *
  * Logic analyser capture is in the same folder as this example as
  * "multiple_bus_output.png"
- * 
+ *
  * created 30/04/2018 by Alistair Symonds
+ * Modified 19/06/2020 by Glenn Ramalho
+ *    Sampled the returned value from the VSPI and HSPI and printed them to
+ *    STDOUT. Also added prototype declarations for the vspiCommand and
+ *    hspiCommand functions.
  */
 #include <SPI.h>
 
@@ -27,7 +31,7 @@ void setup() {
   //initialise two instances of the SPIClass attached to VSPI and HSPI respectively
   vspi = new SPIClass(VSPI);
   hspi = new SPIClass(HSPI);
-  
+
   //clock miso mosi ss
 
   //initialise vspi with default pins
@@ -35,10 +39,10 @@ void setup() {
   vspi->begin();
   //alternatively route through GPIO pins of your choice
   //hspi->begin(0, 2, 4, 33); //SCLK, MISO, MOSI, SS
-  
+
   //initialise hspi with default pins
   //SCLK = 14, MISO = 12, MOSI = 13, SS = 15
-  hspi->begin(); 
+  hspi->begin();
   //alternatively route through GPIO pins
   //hspi->begin(25, 26, 27, 32); //SCLK, MISO, MOSI, SS
 
@@ -63,7 +67,7 @@ void vspiCommand() {
   //use it as you would the regular arduino SPI API
   vspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
   digitalWrite(5, LOW); //pull SS slow to prep other end for transfer
-  vspi->transfer(data);  
+  data = vspi->transfer(data);
   digitalWrite(5, HIGH); //pull ss high to signify end of data transfer
   vspi->endTransaction();
   /* Upon success, we print a message, but only once. */
@@ -72,10 +76,10 @@ void vspiCommand() {
 
 void hspiCommand() {
   byte stuff = 0b11001100;
-  
+
   hspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
   digitalWrite(15, LOW);
-  hspi->transfer(stuff);
+  stuff = hspi->transfer(stuff);
   digitalWrite(15, HIGH);
   hspi->endTransaction();
   /* Upon success, we print a message, but only once. */
